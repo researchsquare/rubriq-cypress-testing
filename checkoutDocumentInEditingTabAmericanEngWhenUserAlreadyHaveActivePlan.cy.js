550cb73cccbd07e69cc4dd6410f3e17e02789927
Ajith2223 <ajith.a@mpslimited.com>
2025-04-29 12:53:39 +0000

    Modified Checkout functionality

--- checkoutDocumentInEditingTabAmericanEngWhenUserAlreadyHaveActivePlan.cy.js
+++
@@ -1,5 +0,0 @@
-Can't run because no spec files were found.
-
-We searched for specs matching this glob pattern:
-
-  > /home/ajitha/rs/ares/rubriq-cypress-testing/cypress/e2e/rubriq/CheckOutDocument/rubriq
